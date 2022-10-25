b0890919a50342751c782436310721c917e3cdda
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-10-25 11:26:06 -0400

    Update herewego.py

--- methods/commands/herewego.py
+++ methods/commands/herewego.py
@@ -4,6 +4,9 @@ from PIL import Image
 class HereWeGo(ImageCommand):
 	DESCRIPTION = 'Sends an "Ah shit, here we go again" meme'
 
+	def has_args(self, query):
+		return True
+		
 	def response(self):
 		image = Image.open("assets/herewego.png")
 		return "", self.upload_pil_image(image, "GIF")
