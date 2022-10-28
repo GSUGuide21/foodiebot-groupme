9a3c2ef035cc4a961749b41ea6a4144f8f9e2501
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-10-27 22:45:07 -0400

    Fix

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,12 +1,3 @@
-import os
-from selenium import webdriver
 from .commands import commands
 from .system import system
 from .responses import responses
-
-CHROME_OPTIONS = webdriver.ChromeOptions()
-CHROME_OPTIONS.binary_location = os.environ.get("chrome_bin")
-CHROME_OPTIONS.add_argument("--headless")
-CHROME_OPTIONS.add_argument("--disable-dev-shm-usage")
-CHROME_OPTIONS.add_argument("--no-sandbox")
-DRIVER = webdriver.Chrome(executable_path=os.environ.get('chrome_driver_path'), chrome_options=CHROME_OPTIONS)
