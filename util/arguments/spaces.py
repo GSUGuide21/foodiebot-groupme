ab5f34b5f93bbe344015664c40ad07f7db4835fa
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-11-07 13:31:25 -0500

    Adding Pipe Arguments

--- util/arguments/spaces.py
+++ util/arguments/spaces.py
@@ -3,5 +3,5 @@ from .split import SplitArgument
 
 class SpacesArgument(SplitArgument):
 	def __init__(self, **options):
-		delimiter = compile(r"\|", flags=MULTILINE)
+		delimiter = compile(r"\s", flags=MULTILINE)
 		super().__init__(delimiter=delimiter, split_type="pattern", **options)
