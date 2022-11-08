39f8a18273b04d54223d4a493d203a474819f052
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-11-08 12:41:32 -0500

    Update base.py

--- util/preconditions/base.py
+++ util/preconditions/base.py
@@ -2,7 +2,7 @@ class Precondition:
 	PRECONDITION_WARNING = "You do not fit the requirements for this command. Try again later :("
 
 	def __init__(self):
-		print(f"Precondition ({self.__name__}) has been loaded!")
+		print(f"Precondition ({self.__class__.__name__}) has been loaded!")
 	
 	def run(self, **options):
 		pass
