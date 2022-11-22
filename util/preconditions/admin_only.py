6b141b329d6d6fdf8b2be9215098832db0610779
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-11-22 00:44:13 -0500

    Update admin_only.py

--- util/preconditions/admin_only.py
+++ util/preconditions/admin_only.py
@@ -1,9 +1,11 @@
-from .base import Precondition
-from manager import Group
+import manager
+from .base import *
+# from .base import Precondition
+# from manager import Group
 
 class AdminOnly(Precondition):
 	def run(self, **options):
-		group: Group = options["group"]
+		group: manager.Group = options["group"]
 		user_id = options["message"]["user_id"]
 		admins = [admin_user_id for admin_user_id in group.admins.keys()]
 		return user_id in admins
