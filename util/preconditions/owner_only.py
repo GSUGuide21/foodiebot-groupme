fbc92764d6cc3131067785c08627901f80f0b820
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-11-22 00:47:07 -0500

    Update owner_only.py

--- util/preconditions/owner_only.py
+++ util/preconditions/owner_only.py
@@ -1,12 +1,12 @@
-from ..group import Group
-from .base import Precondition
+import manager
+from .base import *
 
 class OwnerOnly(Precondition):
 	def __init__(self):
 		pass
 
 	def run(self, **options):
-		group: Group = options["group"]
+		group: manager.Group = options["group"]
 		user_id = options["message"]["user_id"]
 		owner = group.owner
 		return owner.user_id == user_id
