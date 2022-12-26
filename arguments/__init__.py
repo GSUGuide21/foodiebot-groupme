bfca32a27f74765d519edd28913b3b2da83bc195
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2022-12-25 19:00:55 -0500

    Update __init__.py

--- arguments/__init__.py
+++ arguments/__init__.py
@@ -4,7 +4,7 @@ from importlib import import_module
 
 arguments = {}
 files = [f for f in listdir(dirname(__file__)) if f.endswith(".py")]
-files = list(filter(lambda f: f not in ["__init__.py", "base.py"], files))
+files = list(filter(lambda f: f not in ["__init__.py", "base.py", "split.py"], files))
 
 for file in files:
 	filename = file[0:-len(".py")]
